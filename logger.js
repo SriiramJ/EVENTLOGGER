4ea68a699457af4de9297cafdba3d208116fe46a
Sriram <sriram2005j@gmail.com>
2025-01-13 17:28:33 +0530

    debug done

--- logger.js
+++ logger.js
@@ -1,25 +1,25 @@
-const fs = require('fs')
-const os = require('os')
+const fs = require("fs");
+const os = require("os");
 
-const EventEmitter = require('events')
+const EventEmitter = require("events");
 
-class Logger extends EventEmitter{
-    log(message){
-        this.emit('message',{message})
-    }
+class Logger extends EventEmitter {
+  log(message) {
+    this.emit("message", { message });
+  }
 }
 
-const logger = new Logger()
-const logFile = "./eventlog.txt"
+const logger = new Logger();
+const logFile = "./eventlog.txt";
 
-const logToFile = (event) =>{
-    const logMessage = `${new Date().toISOString()} - ${event.message}`
-    fs.appendFileSync(logFile, logMessage)
-}
+const logToFile = (event) => {
+  const logMessage = `${new Date().toISOString()} - ${event.message}`;
+  fs.appendFileSync(logFile, logMessage);
+};
 
-logger.on('message', logToFile)
+logger.on("message", logToFile);
 
-setInterval(()=>{
-    const memoryUsage = (os.freemem() / os.totalmem()) * 100
-    logger.log(`Current memory usage: ${memoryUsage.toFixed(2)}`)
-},3000)
+setInterval(() => {
+  const memoryUsage = (os.freemem() / os.totalmem()) * 100;
+  logger.log(`Current memory usage: ${memoryUsage.toFixed(2)}`);
+}, 3000);
