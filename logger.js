ea76eb8148d5e294f9971f10931cdb0603e8894d
Sriram <sriram2005j@gmail.com>
2025-01-13 17:27:55 +0530

    add execution to the codebase

--- logger.js
+++ logger.js
@@ -18,3 +18,8 @@ const logToFile = (event) =>{
 }
 
 logger.on('message', logToFile)
+
+setInterval(()=>{
+    const memoryUsage = (os.freemem() / os.totalmem()) * 100
+    logger.log(`Current memory usage: ${memoryUsage.toFixed(2)}`)
+},3000)
